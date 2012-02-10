8097422895e0d3e533448d1f7588ea496fced659
andreasfrom <andro.from@gmail.com>
2012-02-10 20:27:42 +0000

    "Replaced foo['bar'] with foo.bar"

--- Snake.js
+++ Snake.js
@@ -1,25 +1,25 @@
-GetData = function()
+function GetData()
 {
-  Output = [];
+  var Output = [];
 
-  Output["MultiPlayer"] = document.getElementById("multiplayer").checked;
-  Output["Canvas"] = document.getElementById("canvas");
-  Output["Timer"] = document.getElementById("timer");
-  Output["TimerInput"] = document.getElementById("timerinput").value;
-  Output["Controls1"] = document.getElementById("controls1");
-  Output["Controls2"] = document.getElementById("controls2");
-  Output["Name1"] = document.getElementById("name1").value;
-  Output["Name2"] = document.getElementById("name2").value;
-  Output["Color1"] = document.getElementById("color1").value;
-  Output["Color2"] = document.getElementById("color2").value;
-  Output["Score"] = document.getElementById("score");
-  Output["Score1"] = document.getElementById("score1");
-  Output["Score2"] = document.getElementById("score2");
+  Output.MultiPlayer = document.getElementById("multiplayer").checked;
+  Output.Canvas = document.getElementById("canvas");
+  Output.Timer = document.getElementById("timer");
+  Output.TimerInput = document.getElementById("timerinput").value;
+  Output.Controls1 = document.getElementById("controls1");
+  Output.Controls2 = document.getElementById("controls2");
+  Output.Name1 = document.getElementById("name1").value;
+  Output.Name2 = document.getElementById("name2").value;
+  Output.Color1 = document.getElementById("color1").value;
+  Output.Color2 = document.getElementById("color2").value;
+  Output.Score = document.getElementById("score");
+  Output.Score1 = document.getElementById("score1");
+  Output.Score2 = document.getElementById("score2");
 
   return Output;
 }
 
-DisplayPlayer2 = function(value) {
+function DisplayPlayer2(value) {
   var Controls2 = document.getElementById("controls2");
   if (value)
     Controls2.setAttribute("style","visibility: visible");
@@ -27,36 +27,36 @@ DisplayPlayer2 = function(value) {
     Controls2.setAttribute("style","visibility: hidden");
 }
 
-StartGame = function () {
-  Data = GetData();
+function StartGame() {
+  var Data = GetData();
 
-  Snake = SnakeLib;
-  Snake.HTML.Canvas = Data['Canvas'];
-  Snake.HTML.Score1 = Data['Score1'];
-  Snake.HTML.Score2 = Data['Score2'];
-  Snake.HTML.Timer = Data['Timer'];
+  var Snake = SnakeLib;
+  Snake.HTML.Canvas = Data.Canvas;
+  Snake.HTML.Score1 = Data.Score1;
+  Snake.HTML.Score2 = Data.Score2;
+  Snake.HTML.Timer = Data.Timer;
 
-  Snake.Init(Data['MultiPlayer']);
+  Snake.Init(Data.MultiPlayer);
 
-  Snake.Settings.Timer = Data['TimerInput'];
+  Snake.Settings.Timer = Data.TimerInput;
   Snake.Settings.ShowTimer = true;
   Snake.Settings.ShowScore = true;
 
-  if (Data['MultiPlayer']) {
-    Data['Score2'].setAttribute("style","color:"+Data['Color2']);
+  if (Data.MultiPlayer) {
+    Data.Score2.setAttribute("style","color:"+Data.Color2);
 
-    Snake.Player2.Name = Data['Name2'];
-    Snake.Player2.Color = Data['Color2'];
+    Snake.Player2.Name = Data.Name2;
+    Snake.Player2.Color = Data.Color2;
   }
 
-  Data['Score1'].setAttribute("style","color:"+Data['Color1']);
+  Data.Score1.setAttribute("style","color:"+Data.Color1);
 
   document.body.removeChild(document.getElementById("controls"));
 
-  Data['Score'].setAttribute("style","visibility:visible");
+  Data.Score.setAttribute("style","visibility:visible");
 
-  Snake.Player.Name = Data['Name1'];
-  Snake.Player.Color = Data['Color1'];
+  Snake.Player.Name = Data.Name1;
+  Snake.Player.Color = Data.Color1;
 
   Snake.StartGame();
 }
